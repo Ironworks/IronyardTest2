a3fb4b9b71a5d9d65592a0dfa2ecc7912102ba4e
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2016-04-27 16:31:47 +0100

    TipCalc create

---
+++ IronyardTest2/TipCalc.h
@@ -0,0 +1,13 @@
+//
+//  TipCalc.h
+//  IronyardTest2
+//
+//  Created by Trevor Doodes on 27/04/2016.
+//  Copyright © 2016 Ironworks Media Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TipCalc : NSObject
+
+@end
---
+++ IronyardTest2/TipCalc.m
@@ -0,0 +1,13 @@
+//
+//  TipCalc.m
+//  IronyardTest2
+//
+//  Created by Trevor Doodes on 27/04/2016.
+//  Copyright © 2016 Ironworks Media Limited. All rights reserved.
+//
+
+#import "TipCalc.h"
+
+@implementation TipCalc
+
+@end
